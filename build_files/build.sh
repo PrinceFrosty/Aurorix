58fa7a7bdfba466e3c1d07a2677224cf8b7f80aa
Christopher Bowens <frosty@frostyfort.net>
2026-07-03 22:46:40 -0400

    install lutris

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 dnf5 copr enable -y scottames/ghostty
-dnf5 install -y steam ghostty nix btrfs-assistant 'https://github.com/edde746/plezy/releases/latest/download/plezy-linux-x64.rpm'
+dnf5 install -y steam ghostty lutris nix btrfs-assistant 'https://github.com/edde746/plezy/releases/latest/download/plezy-linux-x64.rpm'
 dnf5 copr disable -y scottames/ghostty
 
 # Install RPM
